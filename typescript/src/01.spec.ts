47bccaa1153e9f4316de7e5f872f0b7083ad1053
Jack Rose <user@jackrose.co.nz>
2023-07-05 21:04:28 +1000

    ts: day 1 part 2

--- typescript/src/01.spec.ts
+++ typescript/src/01.spec.ts
@@ -1,13 +1,24 @@
 import * as fs from "fs/promises";
-import { part1 } from "./01";
+import { part1, part2 } from "./01";
 
-test("part 1 test input", () => {
-  expect(part1(testInput)).toBe(24000);
-});
-
-test("part 1 real input", async () => {
+describe("day 1", async () => {
   const realInput = await fs.readFile("../inputs/day01.txt", "utf8");
-  expect(part1(realInput)).toBe(71934);
+
+  test("part 1 test input", () => {
+    expect(part1(testInput)).toBe(24000);
+  });
+
+  test("part 1 real input", () => {
+    expect(part1(realInput)).toBe(71934);
+  });
+
+  test("part 2 test input", () => {
+    expect(part2(testInput)).toBe(45000);
+  });
+
+  test("part 2 real input", () => {
+    expect(part2(realInput)).toBe(211447);
+  });
 });
 
 const testInput = `
--- typescript/src/01.ts
+++ typescript/src/01.ts
@@ -1,12 +1,20 @@
 import * as R from "ramda";
 
-export function part1(input: string) {
-  const groups = input
+function sumGroups(input: string) {
+  const sums = input
     .trim()
     .split("\n\n")
-    .map((group) => group.split("\n").map((line) => parseInt(line)));
+    .map((group) => group.split("\n").map((line) => parseInt(line)))
+    .map(R.sum);
 
-  const sums = groups.map(R.sum);
-  const max = Math.max(...sums);
-  return max;
+  return R.sort((a, b) => b - a, sums);
+}
+
+export function part1(input: string) {
+  return sumGroups(input)[0];
+}
+
+export function part2(input: string) {
+  const groups = sumGroups(input);
+  return groups[0] + groups[1] + groups[2];
 }
