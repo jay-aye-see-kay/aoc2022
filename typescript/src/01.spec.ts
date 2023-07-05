5524e914c172ce0cec8e7b7d03755c80bddb86b3
Jack Rose <user@jackrose.co.nz>
2023-07-05 20:52:53 +1000

    ts: day 1 part 1

--- typescript/src/01.spec.ts
+++ typescript/src/01.spec.ts
@@ -1,5 +1,28 @@
+import * as fs from "fs/promises";
 import { part1 } from "./01";
 
-test("works", () => {
-  expect(part1()).toBe(true);
+test("part 1 test input", () => {
+  expect(part1(testInput)).toBe(24000);
 });
+
+test("part 1 real input", async () => {
+  const realInput = await fs.readFile("../inputs/day01.txt", "utf8");
+  expect(part1(realInput)).toBe(71934);
+});
+
+const testInput = `
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+`;
--- typescript/src/01.ts
+++ typescript/src/01.ts
@@ -1,3 +1,12 @@
-export function part1() {
-  return true;
+import * as R from "ramda";
+
+export function part1(input: string) {
+  const groups = input
+    .trim()
+    .split("\n\n")
+    .map((group) => group.split("\n").map((line) => parseInt(line)));
+
+  const sums = groups.map(R.sum);
+  const max = Math.max(...sums);
+  return max;
 }
